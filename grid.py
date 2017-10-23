2e76ab430398382502e02e9b1af607915540912c
PiotrChromniak <Piotr.Chromniak@gmail.com>
2017-10-23 23:07:26 +0200

    Added grid generation

---
+++ grid.py
@@ -0,0 +1,58 @@
+import ranges as rg
+
+def generate_ranges(points, elements):
+
+  ranges = []
+  for element in elements:
+    (p1, p2, p3) = (points[element[0]], 
+                    points[element[1]], 
+                    points[element[2]])
+    
+    on_the_same_side = (p1[0] > p2[0] and p3[0] > p2[0]) or (p1[0] < p2[0] and p3[0] < p2[0])
+
+    if on_the_same_side:
+      ranges.append(rg.OrthogonalRange([p1, p2, p3]))
+    else:
+      ranges.append(rg.StandardRange([p1, p2, p3]))
+    
+  return ranges
+
+def find_grid_borders(points):
+  max_x = points[0][0]
+  min_x = points[0][0]
+  max_y = points[0][1]
+  min_y = points[0][1]
+
+  for point in points[1:]:
+    max_x = max(max_x, point[0])
+    min_x = min(min_x, point[1])
+    max_y = max(max_y, point[0])
+    min_y = min(min_y, point[1])
+
+  return (min_x, max_x, min_y, max_y)
+
+def generate_grid_points(ranges, grid_density, grid_borders):
+  (min_x, max_x, min_y, max_y) = (grid_borders['min_x'], grid_borders['max_x'], 
+                                  grid_borders['min_y'], grid_borders['max_y'])
+
+  dx = (max_x - min_x) / grid_density
+  y_density = int((max_y - min_y) / dx)
+
+  points = []
+  for i in range(grid_density):
+    current_x = min_x + dx/2 + i * dx
+    for j in range(y_density):
+      current_y = min_y + dx/2 + j * dx
+      
+      valid_ranges = [f_range for f_range in ranges if f_range.is_in_range(current_x)]
+      intersection_count = sum([f_range.get_intersections_count(current_x, current_y) for f_range in valid_ranges])
+      if intersection_count % 2 is 1:
+        points.append([current_x, current_y])
+        
+  return points
+'''
+a = 10
+dict1 = {'min_x': 0.0, 'max_x': 20.0, 'min_y': 0.0, 'max_y': 10.0}
+
+generate_grid_points(None, a, dict1)
+'''
