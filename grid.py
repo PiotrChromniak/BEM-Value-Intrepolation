3d4b4f4c995b71e97c0a07da64229807afc48ea8
PiotrChromniak <Piotr.Chromniak@gmail.com>
2017-10-26 20:37:38 +0200

    heh

--- grid.py
+++ grid.py
@@ -1,74 +1,87 @@
 import ranges as rg
 
+
 def generate_ranges(points, elements):
+    ranges = []
+    for element in elements:
+        (p1, p2, p3) = (points[element[0]],
+                        points[element[1]],
+                        points[element[2]])
 
-  ranges = []
-  for element in elements:
-    (p1, p2, p3) = (points[element[0]], 
-                    points[element[1]], 
-                    points[element[2]])
-    
-    on_the_same_side = (p1[0] > p2[0] and p3[0] > p2[0]) or (p1[0] < p2[0] and p3[0] < p2[0])
+        on_the_same_side = (p1[0] > p2[0] and p3[0] > p2[0]) or (
+            p1[0] < p2[0] and p3[0] < p2[0])
 
-    if on_the_same_side:
-      ranges.append(rg.OrthogonalRange([p1, p2, p3]))
-    else:
-      ranges.append(rg.StandardRange([p1, p2, p3]))
-    
-  return ranges
+        if on_the_same_side:
+            ranges.append(rg.OrthogonalRange([p1, p2, p3]))
+        else:
+            ranges.append(rg.StandardRange([p1, p2, p3]))
+
+    return ranges
+
 
 def find_grid_borders(points):
-  max_x = points[0][0]
-  min_x = points[0][0]
-  max_y = points[0][1]
-  min_y = points[0][1]
+    max_x = points[0][0]
+    min_x = points[0][0]
+    max_y = points[0][1]
+    min_y = points[0][1]
 
-  for point in points[1:]:
-    max_x = max(max_x, point[0])
-    min_x = min(min_x, point[0])
-    max_y = max(max_y, point[1])
-    min_y = min(min_y, point[1])
+    for point in points[1:]:
+        max_x = max(max_x, point[0])
+        min_x = min(min_x, point[0])
+        max_y = max(max_y, point[1])
+        min_y = min(min_y, point[1])
 
-  return { 'min_x': min_x, 'max_x': max_x, 'min_y': min_y, 'max_y': max_y }
+    return {'min_x': min_x, 'max_x': max_x, 'min_y': min_y, 'max_y': max_y}
+
 
 def generate_grid_points(ranges, grid_density, grid_borders):
-  (min_x, max_x, min_y, max_y) = (grid_borders['min_x'], grid_borders['max_x'], 
-                                  grid_borders['min_y'], grid_borders['max_y'])
+    (min_x, max_x, min_y, max_y) = (grid_borders['min_x'], grid_borders['max_x'],
+                                    grid_borders['min_y'], grid_borders['max_y'])
 
-  dx = (max_x - min_x) / grid_density
-  y_density = int((max_y - min_y) / dx)
+    dx = (max_x - min_x) / grid_density
+    y_density = int((max_y - min_y) / dx)
 
-  points = []
-  for i in range(grid_density):
-    current_x = min_x + dx/2 + i * dx
-    for j in range(y_density):
-      current_y = min_y + dx/2 + j * dx
-      
-      valid_ranges = [f_range for f_range in ranges if f_range.is_in_range(current_x)]
-      intersection_count = sum([f_range.get_intersections_count(current_x, current_y) for f_range in valid_ranges])
-      if intersection_count % 2 is 1:
-        points.append([current_x, current_y])
-        
-  return points
+    points = []
+    for i in range(grid_density):
+        current_x = min_x + dx / 2 + i * dx
+        for j in range(y_density):
+            current_y = min_y + dx / 2 + j * dx
+
+            valid_ranges = [
+                f_range for f_range in ranges if f_range.is_in_range(current_x)]
+            intersection_counters = [f_range.get_intersections_count(
+                current_x, current_y) for f_range in valid_ranges]
+            intersection_count = sum(intersection_counters)
+            if intersection_count % 2 is 1:
+                points.append([current_x, current_y])
+
+    return points
+
 
 def split_points(points, elements):
-  pass
+    pass
 
-'''
-#przykład
+
+# przykład
 import math
 a = 0.785398163397
 r = 5
-points = [[math.cos(i*a) * r, math.sin(i*a) * r] for i in range(8)]
-elements = [[7,0,1],
-            [3,4,5],
-            [5,6,7],
-            [1,2,3]]
+r2 = 2
+points = [[math.cos(i * a) * r, math.sin(i * a) * r] for i in range(8)]
+points2 = [[math.cos(i * a) * r2, math.sin(i * a) * r2] for i in range(8)]
+points = points + points2
+elements = [[7, 0, 1],
+            [3, 4, 5],
+            [5, 6, 7],
+            [1, 2, 3],
+            [8, 9, 10],
+            [10, 11, 12],
+            [12, 13, 14],
+            [14, 15, 16]]
 
 ranges = generate_ranges(points, elements)
 borders = find_grid_borders(points)
-grid_points = generate_grid_points(ranges,10,borders)
+grid_points = generate_grid_points(ranges, 10, borders)
 
 for point in grid_points:
-  print("r: {0}".format(math.sqrt(point[0]**2 + point[1]**2)))
-'''
+    print('r: {:.2}'.format(math.sqrt(point[0]**2 + point[1]**2)))
--- ranges.py
+++ ranges.py
@@ -1,89 +1,93 @@
 import numpy as np
 import math
 
+
 class FunctionRange(object):
-  def __init__(self, ans):
-    self.A = float(ans[0])
-    self.B = float(ans[1])
-    self.C = float(ans[2])
+    def __init__(self, ans):
+        self.A = float(ans[0])
+        self.B = float(ans[1])
+        self.C = float(ans[2])
 
-  def is_in_range(self, x):
-    raise NotImplementedError()
+    def is_in_range(self, x):
+        raise NotImplementedError()
 
-  def get_intersections_count(self, x, limit):
-    raise NotImplementedError()
+    def get_intersections_count(self, x, limit):
+        raise NotImplementedError()
 
 
 class StandardRange(FunctionRange):
-  def __init__(self, points):
-    (x1, y1, x2, y2, x3, y3) = (points[0][0], points[0][1],
-                                points[1][0], points[1][1],
-                                points[2][0], points[2][1])
-    mat = np.array([[x1**2, x1, 1],
-                    [x2**2, x2, 1],
-                    [x3**2, x3, 1]])
-    b = np.array([y1, y2, y3])
-    ans = np.linalg.solve(mat, b)
-    FunctionRange.__init__(self, ans)
+    def __init__(self, points):
+        (x1, y1, x2, y2, x3, y3) = (points[0][0], points[0][1],
+                                    points[1][0], points[1][1],
+                                    points[2][0], points[2][1])
+        mat = np.array([[x1**2, x1, 1],
+                        [x2**2, x2, 1],
+                        [x3**2, x3, 1]])
+        b = np.array([y1, y2, y3])
+        ans = np.linalg.solve(mat, b)
+        FunctionRange.__init__(self, ans)
 
-    self.start = min(x1, x3)
-    self.end = max(x1, x3)
+        self.start = min(x1, x3)
+        self.end = max(x1, x3)
 
-  def _f(self, x):
-    return ((self.A * x) + self.B) * x + self.C
+    def _f(self, x):
+        return ((self.A * x) + self.B) * x + self.C
 
-  def is_in_range(self, x):
-    return x >= self.start and x <= self.end
+    def is_in_range(self, x):
+        return x >= self.start and x <= self.end
 
-  def get_intersections_count(self, x, limit):
-    y = self._f(x)
-    if y < limit:
-      return 0
-    else:
-      return 1
+    def get_intersections_count(self, x, limit):
+        y = self._f(x)
+        if y < limit:
+            return 0
+        else:
+            return 1
+
 
 class OrthogonalRange(FunctionRange):
-  def __init__(self, points):
-    (x1, y1, x2, y2, x3, y3) = (points[0][0], points[0][1],
-                                points[1][0], points[1][1],
-                                points[2][0], points[2][1])
-    mat = np.array([[y1**2, y1, 1],
-                    [y2**2, y2, 1],
-                    [y3**2, y3, 1]])
-    b = np.array([x1, x2, x3])
-    ans = np.linalg.solve(mat, b)
-    FunctionRange.__init__(self, ans)
+    def __init__(self, points):
+        (x1, y1, x2, y2, x3, y3) = (points[0][0], points[0][1],
+                                    points[1][0], points[1][1],
+                                    points[2][0], points[2][1])
+        mat = np.array([[y1**2, y1, 1],
+                        [y2**2, y2, 1],
+                        [y3**2, y3, 1]])
+        b = np.array([x1, x2, x3])
+        ans = np.linalg.solve(mat, b)
+        FunctionRange.__init__(self, ans)
 
-    symm_x = (4*self.A*self.C -self.B**2)/(4*self.A)
-    upper_x = x1 if y1>y3 else x3
-    lower_x = x3 if y1>y3 else x1
-    self.upper_end = max(symm_x, upper_x)
-    self.upper_start = min(symm_x, upper_x)
-    self.lower_end = max(symm_x, lower_x)
-    self.lower_start = min(symm_x, lower_x)
+        symm_x = (4 * self.A * self.C - self.B**2) / (4 * self.A)
+        upper_x = x1 if y1 > y3 else x3
+        lower_x = x3 if y1 > y3 else x1
+        self.upper_end = max(symm_x, upper_x)
+        self.upper_start = min(symm_x, upper_x)
+        self.lower_end = max(symm_x, lower_x)
+        self.lower_start = min(symm_x, lower_x)
 
-  def is_in_range(self, x):
-    in_upper_range = x >= self.upper_start and x <= self.upper_end
-    in_lower_range = x >= self.lower_start and x <= self.lower_end
-    return in_lower_range or in_upper_range
-    
-  def get_intersections_count(self, x, limit):
-    sqrt_delta = math.sqrt(self.B**2 - 4 * self.A * (self.C - x))
-    (solution1, solution2) = ((-self.B-sqrt_delta)/(2*self.A), (-self.B+sqrt_delta)/(2*self.A))
-    y1 = min(solution1, solution2)
-    y2 = max(solution1, solution2)
-    
-    intersection_counter = 0
+    def is_in_range(self, x):
+        in_upper_range = x >= self.upper_start and x <= self.upper_end
+        in_lower_range = x >= self.lower_start and x <= self.lower_end
+        return in_lower_range or in_upper_range
 
-    if x >= self.lower_start and x <= self.lower_end and y1 >= limit:
-      intersection_counter += 1
-    
-    if x >= self.upper_start and x <= self.upper_end and y2 >= limit:
-      intersection_counter += 1
-    
-    return intersection_counter
+    def get_intersections_count(self, x, limit):
+        sqrt_delta = math.sqrt(self.B**2 - 4 * self.A * (self.C - x))
+        (solution1, solution2) = ((-self.B - sqrt_delta) /
+                                  (2 * self.A), (-self.B + sqrt_delta) / (2 * self.A))
+        y1 = min(solution1, solution2)
+        y2 = max(solution1, solution2)
 
-'''
+        intersection_counter = 0
+
+        if x >= self.lower_start and x <= self.lower_end and y1 >= limit:
+            intersection_counter += 1
+
+        if x >= self.upper_start and x <= self.upper_end and y2 >= limit:
+            intersection_counter += 1
+
+        return intersection_counter
+
+
+
 a = OrthogonalRange([[2,3], [3,2], [1,1]])
 ans1 = a.get_intersections_count(1.5, 0.5)
 ans2 = a.get_intersections_count(1.5, 2)
@@ -102,4 +106,4 @@ b1 = b.is_in_range(-2)
 b2 = b.is_in_range(0.5)
 b3 = b.is_in_range(2)
 print("a")
-'''
+
