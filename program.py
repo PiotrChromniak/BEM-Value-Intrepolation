1701d5ff9816e864829e0c9c3b989c7b0e87d3bb
PiotrChromniak <Piotr.Chromniak@gmail.com>
2017-11-25 14:12:12 +0100

    Removed redundant list creations and switched to dense ndarrays

--- program.py
+++ program.py
@@ -6,10 +6,14 @@ import argparse
 import os.path
 
 parser = argparse.ArgumentParser(description="Arguments for paser")
-parser.add_argument('-p', default=2, type=int, help='[uint] Factor for interpolation', metavar='COUNT')
-parser.add_argument('-grid', type=int, required=True, help='[uint] Grid density', metavar='COUNT')
-parser.add_argument('-file', type=argparse.FileType('r'), required=True, help='Input file', metavar='PATH')
-parser.add_argument('-plotdir', type=str, default='', help='Folder to hold output plots', metavar='DIRPATH')
+parser.add_argument('-p', default=2, type=int,
+                    help='[uint] Factor for interpolation', metavar='COUNT')
+parser.add_argument('-grid', type=int, required=True,
+                    help='[uint] Grid density', metavar='COUNT')
+parser.add_argument('-file', type=argparse.FileType('r'),
+                    required=True, help='Input file', metavar='PATH')
+parser.add_argument('-plotdir', type=str, default='',
+                    help='Folder to hold output plots', metavar='DIRPATH')
 parser.add_argument('-out', type=str, help='Name of vtk output file')
 args = parser.parse_args()
 
@@ -22,43 +26,45 @@ ans2 = interpolation.interpolate(data, args.grid, args.p)
 
 ans = ans2['values']
 
-x = np.array([ i[0] for i in data['points']] + [ i[0] for i in data['internal_points']] + [ i[0] for i in ans2['points']])
-y = np.array([ i[1] for i in data['points']] + [ i[1] for i in data['internal_points']] + [ i[1] for i in ans2['points']])
-z = np.array(data['Sxx'] + data['inSxx'] + ans['Sxx'])
+x = np.array([i[0] for i in data['points']] + [i[0]
+             for i in data['internal_points']] + [i[0] for i in ans2['points']])
+y = np.array([i[1] for i in data['points']] + [i[1]
+             for i in data['internal_points']] + [i[1] for i in ans2['points']])
+Sxx = np.array(data['Sxx'] + data['inSxx'] + ans['Sxx'])
 
-print('Generating plots...',end='')
-plt.tripcolor(x, y, z, cmap=plt.cm.hot_r, edgecolor='black')
-#plt.tricontourf(x, y, z, 20, cmap=plt.cm.hot_r,edgecolor='black')
+print('Generating plots...', end='')
+plt.tripcolor(x, y, Sxx, cmap=plt.cm.hot_r, edgecolor='black')
+# plt.tricontourf(x, y, z, 20, cmap=plt.cm.hot_r,edgecolor='black')
 cbar = plt.colorbar()
 path = args.plotdir
-plt.savefig(os.path.join(path,'Sxx'))
+plt.savefig(os.path.join(path, 'Sxx'))
 
 
 cbar.remove()
-z = np.array(data['Syy'] + data['inSyy'] + ans['Syy'])
-plt.tripcolor(x, y, z, cmap=plt.cm.hot_r, edgecolor='black')
-#plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
+Syy = np.array(data['Syy'] + data['inSyy'] + ans['Syy'])
+plt.tripcolor(x, y, Syy, cmap=plt.cm.hot_r, edgecolor='black')
+# plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
 cbar = plt.colorbar()
 plt.savefig(os.path.join(path, 'Syy'))
 
 cbar.remove()
-z = np.array(data['Sxy'] + data['inSxy'] + ans['Sxy'])
-plt.tripcolor(x, y, z, cmap=plt.cm.hot_r, edgecolor='black')
-#plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
+Sxy = np.array(data['Sxy'] + data['inSxy'] + ans['Sxy'])
+plt.tripcolor(x, y, Sxy, cmap=plt.cm.hot_r, edgecolor='black')
+# plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
 cbar = plt.colorbar()
 plt.savefig(os.path.join(path, 'Sxy'))
 
 cbar.remove()
-z = np.array(data['UX'] + data['inUX'] + ans['UX'])
-plt.tripcolor(x, y, z, cmap=plt.cm.hot_r, edgecolor='black')
-#plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
+UX = np.array(data['UX'] + data['inUX'] + ans['UX'])
+plt.tripcolor(x, y, UX, cmap=plt.cm.hot_r, edgecolor='black')
+# plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
 cbar = plt.colorbar()
 plt.savefig(os.path.join(path, 'UX'))
 
 cbar.remove()
-z = np.array(data['UY'] + data['inUY'] + ans['UY'])
-plt.tripcolor(x, y, z, cmap=plt.cm.hot_r, edgecolor='black')
-#plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
+UY = np.array(data['UY'] + data['inUY'] + ans['UY'])
+plt.tripcolor(x, y, UY, cmap=plt.cm.hot_r, edgecolor='black')
+# plt.tricontourf(x, y, z, 10, cmap=plt.cm.hot_r,edgecolor='black')
 cbar = plt.colorbar()
 plt.savefig(os.path.join(path, 'UY'))
 print('done')
@@ -71,25 +77,43 @@ if args.out:
     grid = ans2['points']
     grid_count = len(grid)
     elements = data['elements']
-    Sxx = data['Sxx'] + data['inSxx'] + ans['Sxx']
-    Syy = data['Syy'] + data['inSyy'] + ans['Syy']
-    Sxy = data['Sxy'] + data['inSxy'] + ans['Sxy']
 
     print('Writing vtk files..', end='')
-    with open(args.out + '.vtk', 'w', encoding = 'utf-8') as file:
+    with open(args.out + '0' + '.vtk', 'w', encoding='utf-8') as file:
         file.write('# vtk DataFile Version 2.0\n')
-        file.write('test\n')
+        file.write('ASCII\n')
+        file.write('DATASET UNSTRUCTURED_GRID\n')
+        file.write('POINTS {} double\n'.format(
+            points_count + internal_count + grid_count))
+        for pt_x, pt_y in zip(x, y):
+            file.write('{0} {1} 0.0\n'.format(pt_x, pt_y))
+
+        file.write('CELLS {0} {1}\n'.format(len(elements), len(elements) * 4))
+        for el in elements:
+            file.write('3 {0} {1} {2}\n'.format(el[0], el[1], el[2]))
+
+        file.write('CELL_TYPES {}\n'.format(len(elements)))
+        for _ in range(len(elements)):
+            file.write('21\n')
+
+        file.write('POINT_DATA {0}\nTENSORS stresses double\n'.format(
+            points_count + internal_count + grid_count))
+
+        for xx, yy, xy in zip(Sxx, Syy, Sxy):
+            file.write(
+                '{0} {2} 0.0\n{2} {1} 0.0\n0.0 0.0 0.0\n'.format(xx, yy, xy))
+            file.write('\n')
+    
+    x = x + UX
+    y = y + UY
+    with open(args.out + '1' + '.vtk', 'w', encoding = 'utf-8') as file:
+        file.write('# vtk DataFile Version 2.0\n')
+        file.write('Deformed mesh\n')
         file.write('ASCII\n')
         file.write('DATASET UNSTRUCTURED_GRID\n')
         file.write('POINTS {} double\n'.format(points_count + internal_count + grid_count))
-        for pt in points:
-            file.write('{0} {1} 0.0\n'.format(pt[0], pt[1]))
-
-        for pt in internal:
-            file.write('{0} {1} 0.0\n'.format(pt[0], pt[1]))
-
-        for pt in grid:
-            file.write('{0} {1} 0.0\n'.format(pt[0], pt[1]))
+        for pt_x, pt_y in zip(x,y):
+            file.write('{0} {1} 0.0\n'.format(pt_x, pt_y))
 
         file.write('CELLS {0} {1}\n'.format(len(elements), len(elements) * 4))
         for el in elements:
@@ -103,6 +127,7 @@ if args.out:
 
         for xx, yy, xy in zip(Sxx, Syy, Sxy):
             file.write('{0} {2} 0.0\n{2} {1} 0.0\n0.0 0.0 0.0\n'.format(xx, yy, xy))
-            file.write('\n')
+            file.write('\n')   
+
     print('done')
     
